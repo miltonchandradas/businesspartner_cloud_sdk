0ee2cbf4cf8cea34777e2b5d6fdcaf9a26a0b816
Chandradas <milton.chandradas@sap.com>
2022-10-15 01:58:35 -0400

    business-partner controller...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BusinessPartnerController } from './business-partner/business-partner.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BusinessPartnerController],
   providers: [AppService],
 })
 export class AppModule {}
